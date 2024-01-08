2fba6459d97da0b6d4abe750a0f27190b3d61ba9
MiskuZero <739740010@qq.com>
2024-01-09 07:18:32 +0800

    解析方法完成

--- src/util/TreeUtils.java
+++ src/util/TreeUtils.java
@@ -3,32 +3,48 @@ package util;
 import struct.TreeNode;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 
 public class TreeUtils {
 
-    public static String treeAnalysis(TreeNode root) {
-        ArrayDeque<TreeNode> deque = new ArrayDeque<>();
-        deque.add(root);
-        TreeNode block = new TreeNode(0);
-        deque.add(block);
-        StringBuilder result = new StringBuilder();
-        TreeNode temp;
-        while ((temp = deque.poll()) != null) {
-            if (temp == block) {
-                if (deque.isEmpty())
-                    break;
-                result.append("| ");
-                deque.add(block);
+    public static Object[] treeAnalysis(TreeNode root) {
+        ArrayDeque<TreeNode> eleDeque = new ArrayDeque<>();
+        eleDeque.add(root);
+        ArrayList<Integer> valList = new ArrayList<>();
+        int count = 1;
+        TreeNode empty = new TreeNode();
+        TreeNode block = new TreeNode();
+        eleDeque.add(block);
+        StringBuilder posBuilder = new StringBuilder();
+        TreeNode ele;
+        while (count != 0) {
+            if ((ele = eleDeque.poll()) == block) {
+                eleDeque.add(block);
                 continue;
             }
-            result.append(temp.val).append(" ");
-            if (temp.left != null)
-                deque.add(temp.left);
-            if (temp.right != null)
-                deque.add(temp.right);
-
+            if (ele == empty)
+                posBuilder.append(0);
+            else {
+                posBuilder.append(1);
+                assert ele != null;
+                valList.add(ele.val);
+                count--;
+            }
+            if (ele.left == null)
+                eleDeque.add(empty);
+            else {
+                eleDeque.add(ele.left);
+                count++;
+            }
+            if (ele.right == null)
+                eleDeque.add(empty);
+            else {
+                eleDeque.add(ele.right);
+                count++;
+            }
         }
-        return new String(result);
+        while (eleDeque.poll() != block) posBuilder.append(0);
+        return new Object[]{posBuilder, valList};
     }
 
     public static void main(String[] args) {
@@ -40,10 +56,12 @@ public class TreeUtils {
         t.add(0);
         t.add(2);
         t.add(4);
-        t.add(0);
+        t.add(-5);
+        t.add(10);
 
-        String s = TreeUtils.treeAnalysis(t);
-        System.out.println(s);
+        Object[] res = TreeUtils.treeAnalysis(t);
+        System.out.println(res[0]);
+        System.out.println(res[1]);
 
     }
 }
